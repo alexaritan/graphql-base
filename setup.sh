d46959539a2dd175bbcaebed8c230091c6e387c2
Alex Aritan <cokethenwpepsi@gmail.com>
2019-09-01 17:54:35 -0400

    Added babelrc and updated setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Removing association with Github repo"
 #rm -rf ./.git
 
 echo "Adding up-to-date dependencies"
-yarn add graphql-yoga nodemon eslint
+yarn add graphql-yoga nodemon eslint @babel/cli @babel/preset-env @babel/plugin-proposal-object-rest-spread
 
 echo
 echo "HEADS UP: Don't forget to change the URL of the git repo in your package.json"
